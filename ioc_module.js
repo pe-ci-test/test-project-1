ed1e7fe9a8f5247f0fe2ca4b774ddf70f0745b66
Paul <paul.heidenreich@5minds.de>
2017-11-16 15:57:31 +0100

    Initial commit

---
+++ ioc_module.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const schemasIocModule = require('./ioc.schemas');
+
+function registerInContainer (container, registrationSettings) {
+
+  schemasIocModule.registerInContainer(container, registrationSettings);
+}
+
+module.exports.registerInContainer = registerInContainer;
